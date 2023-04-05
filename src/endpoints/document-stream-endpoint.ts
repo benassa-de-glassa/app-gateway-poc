2c263e98851d179a553e0f26cfa1295a5ed2fe04
Benedikt Schmidhuber <schmidhuber.benedikt@gmail.com>
2023-04-05 16:01:53 +0200

    cleanup

--- src/endpoints/document-stream-endpoint.ts
+++ src/endpoints/document-stream-endpoint.ts
@@ -1,7 +1,7 @@
 import { DocumentStreamReadService } from '@benassa-de-glassa/node-utilities/dist/document-service/model/document-stream-read-service.model';
 
 import { StreamEndpointHandler, WebSocketRequest } from '../model/handlers';
-import { Observable, Subject, filter, map, switchMap, tap } from 'rxjs';
+import { Observable, Subject, filter, switchMap } from 'rxjs';
 
 export class DocumentStreamEndpoint implements StreamEndpointHandler {
   public constructor(
@@ -11,19 +11,14 @@ export class DocumentStreamEndpoint implements StreamEndpointHandler {
   public get streamHandler() {
     const documentId$ = new Subject<string>();
     const handleMessage = (request: WebSocketRequest) => {
-      console.log(request);
       documentId$.next((request.queryParameters.id as string) ?? '');
     };
 
     return {
       handleMessage,
       broadcastMessage$: documentId$.pipe(
-        tap(console.log),
         filter((id: string) => id != null),
-        switchMap((id: string) => {
-          return this.documentStreamService.get$(id) as Observable<any>;
-        }),
-        map(doc => JSON.stringify(doc, undefined, 2))
+        switchMap((id: string) => this.documentStreamService.get$(id) as Observable<any>)
       )
     };
   }
